082fe901ab69f4566957f7a817ec5a017b3eab26
Paulo Dias <paulo.dias@outlook.com>
2016-02-05 18:37:37 -0200

    Navigate over questions

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,71 @@
 angular.module('starter.controllers', [])
 
 
-.controller('QuestionsCntrl', function($scope,$http, Questions) {
+.controller('QuestionsCntrl', function($scope,$http, Questions, Alerta, Loading) {
+
+  $scope.currentQuestion = "";
+  $scope.Questions = [];
+  $scope.isLast = false;
+  $scope.isFirst = false;
+  $scope.NextText = "Próxima";
+  $scope.PreviousText = "Anterior";
+  
+ $scope.ValidateLenght =function(){
+     
+     var currentId = $scope.currentQuestion.id;
+     var lenght = $scope.Questions.length;
+     
+     if(currentId == lenght ){
+          $scope.isLast = true;
+          $scope.NextText = "Finalizar";
+     }
+     if(currentId < lenght){
+          $scope.isLast = false;
+          $scope.NextText = "Próxima";
+     }
+
+     if (currentId == 1){
+         $scope.isFirst = true;
+     }
+     else{
+        $scope.isFirst = false; 
+     }
+ }
+  
   $scope.Get = function(){
-    var promise = $http.get('js/questions.json');
-    promise.then(
+      
+    Loading.Show();
+    var pGetJson = $http.get('js/questions.json');
+
+    pGetJson.then(
         function(data) 
         {
              $scope.Questions = data.data;
+             if(data.data.length>0){
+                 // Pego a questão 
+                 $scope.currentQuestion = $scope.Questions[0];
+                 $scope.ValidateLenght();
+                 Loading.Release();
+             }
         });
+  }
   
+  $scope.GoNext = function(id){
+     var question = Questions.get($scope.Questions, id+1);
+     if(question != null){
+         $scope.currentQuestion = question;
+         $scope.ValidateLenght();
+     }
+  }
+  
+  $scope.GoPrevious = function(id){
+     var question = Questions.get($scope.Questions, id-1);
+     if(question != null){
+          $scope.currentQuestion = question;
+         $scope.ValidateLenght();
+    }
   }
   $scope.Get();
-
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Questions) {
-  $scope.chat = Questions.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -14,7 +14,9 @@ var Questions = [];
     remove: function(chat) {
       Questions.splice(Questions.indexOf(chat), 1);
     },
-    get: function(chatId) {
+    get: function(questions, chatId) {
+        
+      Questions = questions;
       for (var i = 0; i < Questions.length; i++) {
         if (Questions[i].id === parseInt(chatId)) {
           return Questions[i];
@@ -23,4 +25,54 @@ var Questions = [];
       return null;
     }
   };
-}]);
+}])
+
+.service('Alerta', function($ionicPopup){
+ 
+    // Alerta padrao
+    var _alert = function(message){
+            $ionicPopup.alert({
+              title: 'DITIL',
+              content: message
+            });
+    };
+    
+    // Mensagem de Erro
+    var _erro = function(message){
+            $ionicPopup.alert({
+              title: 'DITIL',
+              content: message,
+              buttons: [{
+                  text: '<b>OK</b>',
+                  type: 'button-assertive',
+              }]
+            });
+    };
+
+    return {
+        Alerta:_alert,
+        Erro: _erro
+    };
+    
+ })
+ 
+ .service('Loading', function($ionicLoading){
+    
+    // Alerta padrao
+    var _show = function(){
+             $ionicLoading.show({
+                template: 'Carregando...'
+            });
+    };
+    
+    var _release = function(){
+         $ionicLoading.hide();
+    };
+
+
+    return {
+        Show:_show,
+        Release: _release
+    };
+    
+});
