a1e42f6cc9becfcfe0e200aaf83707da4aa6f196
Paulo <paulo.dias@outlook.com>
2016-02-06 18:53:13 -0200

    Terceiro Commit

--- www/js/app.js
+++ www/js/app.js
@@ -5,62 +5,62 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.config(function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+        // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
 
-  // Each tab has its own nav history stack:
+        // Each tab has its own nav history stack:
 
-  .state('tab.questions', {
-    url: '/questions',
-    views: {
-      'tab-questions': {
-        templateUrl: 'templates/tab-questions.html',
-        controller: 'QuestionsCntrl'
-      }
-    }
-  })
+            .state('tab.questions', {
+                url: '/questions',
+                views: {
+                    'tab-questions': {
+                        templateUrl: 'templates/tab-questions.html',
+                        controller: 'QuestionsCntrl'
+                    }
+                }
+            })
 
-  .state('tab.sobre', {
-    url: '/sobre',
-    views: {
-      'tab-sobre': {
-        templateUrl: 'templates/tab-sobre.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+            .state('tab.sobre', {
+                url: '/sobre',
+                views: {
+                    'tab-sobre': {
+                        templateUrl: 'templates/tab-sobre.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/questions');
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/questions');
 
-});
+    });
